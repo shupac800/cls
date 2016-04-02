8bb386c469de727e9ce0cdd3bf664705f6b524d9
shupac800 <davidshuman@gmail.com>
2016-04-02 14:03:49 -0500

    editing text fields in server table working, need to add patch logic for FB

--- javascripts/controllers/ServerCtrl.js
+++ javascripts/controllers/ServerCtrl.js
@@ -1,5 +1,6 @@
-app.controller("ServerCtrl", ["$scope","$http",
-  function($scope,$http) {
+app.controller("ServerCtrl", ["$scope","$http","$compile",
+  function($scope,$http,$compile) {
+
     console.log("ServerCtrl is running");
 
     $http.get("http://cls.firebaseio.com/.json")
@@ -7,11 +8,32 @@ app.controller("ServerCtrl", ["$scope","$http",
       function(response) {
         console.log("read from Firebase:",response.data);
         $scope.searches = response.data;
+        Object.keys($scope.searches).forEach(function(thisKey) {
+          $scope.searches[thisKey].hcreated = new Date($scope.searches[thisKey].created * 1000);
+          $scope.searches[thisKey].hlastsearch = new Date($scope.searches[thisKey].lastsearch * 1000);
+        });
       },
       function(error) {
         console.log("something went awry");
       }
     );
+
+
+    $scope.gotEnter = function(e) {
+      console.log($scope.editText);
+      $("#editing").replaceWith($scope.editText);
+
+    }
+
+    $scope.editCell= function(e) {
+      editing = e;
+      console.log("clicked on",e.target);
+      var oldValue = e.target.innerHTML;
+      //$(e.target).append("<td><input type='text' autofocus=true placeholder='" + oldValue + "' ng-model='newData' ng-keyup='gotKey($event)'>");
+      var el = $compile("<td><input id='editing' type='text' ng-model='editText' ng-keyup='$event.which === 13 && gotEnter()'>")($scope);
+      $(e.target).replaceWith(el);
+    }
+
 
     $scope.addSearch = function($http) {
       console.log($scope.searches);
@@ -24,7 +46,9 @@ app.controller("ServerCtrl", ["$scope","$http",
                             "msgs-sent": 0,
                             "phone": 6155552348,
                             "reported": [],
-                            "searchterm": "spore" };
+                            "searchterm": "spore",
+                            "hcreated": new Date(1459611762000),
+                            "hlastsearch": new Date(1459611762000) };
       $scope.searches.newKey = newData;
     }
 
